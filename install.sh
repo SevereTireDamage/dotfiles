4b5c3a97bb9d3c33b95bb8389131f9189237724a
SevereTireDamage <none>
2017-09-10 15:09:59 -0700

    bash: use a .bashrc-extra instead of a full .bashrc

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 git submodule update --init --recursive
 
+# bash
+cp -b bash/.bashrc-extra ~/.bashrc-extra
+echo source .bashrc-extra >> ~/.bashrc
+
+# vim
 cp -b vimrc/vimrcs/basic.vim ~/.vimrc
-cp -b .bashrc ~/.bashrc
 
